8c72857d52c79885c71050c2c07172eee79f18bb
EZPAY_OA\shang.huang <shang.huang@ezpay.com.tw>
2023-06-19 14:15:22 +0800

    feature: 使用套件

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+import axios from 'axios'
+
+import logo from './assets/logo.svg';
+import './assets/App.css';
+import Input from './components/input';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [text, setText]  = useState('default')
+  const onChangeHandler = (e)=>{
+    setText(e.target.value)
+  }
+  useEffect(()=>{
+  (async ()=>{ // 為何會觸發兩次?
+    const users = await axios.get('https://randomuser.me/api');
+    console.log(users)
+  })()
+  },[])
   return (
     <div className="App">
       <header className="App-header">
+        <Input onChangeHandler={onChangeHandler} id={'haha'} text={text}></Input>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          loreme
         </p>
         <a
           className="App-link"
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './assets/index.css';  // cra環境 可直接匯入css 到 js中
+import App from './App'; // cra 環境中可省略 .js
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // React.StrictMode 會導致 useeffect 執行兩次, 只會在開發模式執行, 會影響運行哪些項目, 平常建議打開
   <React.StrictMode>
     <App />
   </React.StrictMode>
@@ -14,4 +15,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals(); // 這是分析工具(包含GA等) 註解起來也不會怎樣
