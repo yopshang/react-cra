3913915467104aaf9a48d5212cb1ae53339e430f
EZPAY_OA\shang.huang <shang.huang@ezpay.com.tw>
2023-06-27 15:45:53 +0800

    feature: 新增數量修改

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,10 +10,26 @@ export default function Products(){
       productDatas.map(productData=>{
         return (
           <div key={productData.id} className="col">
-            <div className="card">
+            <div  className="card">
                 <img className="card-img-top cardImg" src={productData.img} alt="Card image cap"/>
                 <div className="card-body">
                   <h6 className="card-title d-flex justify-content-between">{productData.title}<span className="">{productData.price}</span></h6>
+                  <select className="form-select mb-2" aria-label="Default select example"
+                   onChange={
+                    (e)=>{
+                      dispatch({
+                        type: 'CHAGE_ADD_AMT',
+                        payload: {
+                          id: productData.id,
+                          amt: e.target.value
+                        }
+                      })
+                    }
+                   }>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                  </select>
                   <button onClick={()=>{
                       dispatch({
                         type: 'ADD_TO_CART',
--- src/store.js
+++ src/store.js
@@ -1,36 +1,58 @@
-import { createContext, useReducer } from 'react';
+import { createContext } from 'react';
 export const CartContext = createContext({})
 export const CartReducer = (state, action)=>{
   const cartList = [...state.cartList]
   const index = cartList.findIndex(item=>item.id === action.payload.id) // 若Id有重複，就不加入，第一次加入一定為 -1
+  const addAmtListIndex = state.addAmtList.findIndex(item=>item.id === action.payload.id)
+  const addAmtList = [...state.addAmtList]
   switch (action.type) {
     case 'ADD_TO_CART':{
       if(index === -1){
         cartList.push(action.payload)
       } else {
-        cartList[index].quantity += action.payload.quantity
+        // cartList[index].quantity += action.payload.quantity
+        cartList[index].quantity += addAmtListIndex===-1?1:parseInt(addAmtList[addAmtListIndex].amt)
+      }
+      console.log('state', state)
+      return {
+        ...state,
+        cartList,
+        addAmtList
+      }
+    }
+    case 'CHAGE_ADD_AMT':
+      if(addAmtListIndex === -1) { // 沒找到 直接新增
+        addAmtList.push({
+          id: action.payload.id,
+          amt: action.payload.amt
+        })
+      } else { // 有找到 修改舊的
+        addAmtList[addAmtListIndex].amt = action.payload.amt
       }
       return {
         ...state,
-        cartList
+        cartList,
+        addAmtList
       }
-    }
     case 'CHANGE_CART_QUANTITY':
       cartList[index].quantity = action.payload.quantity
       return {
         ...state,
-        cartList
+        cartList,
+        addAmtList
       }
     case 'DELETE':
       cartList.splice(index)
       return {
         ...state,
-        cartList
+        cartList,
+        addAmtList
       }
     default:
       return state;
   }
 }
 export const cartInit = {
-  cartList: []
+  cartList: [],
+  addAmtList: []
 }
